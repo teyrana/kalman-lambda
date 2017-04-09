ecdf43592d6eee600b7634fd3df1d15993e65891
Daniel_M_Williams <equipoise@gmail.com>
2017-04-08 21:01:03 -0400

    [feat] implemented initial step of kalman filter, unittest

--- lambda_code.py
+++ lambda_code.py
@@ -1,25 +1,97 @@
 #!python
 from __future__ import print_function
 
+import math
 import json
 import sys
 
 sys.path.append("./libs")
 
-print('Loading function')
+import numpy as np
+
+# CONSTANT PARAMETERS
+
+TRANSITION_MATRIX = np.eye( 2)
+OBSERVATION_MATRIX = np.eye( 2)
+
+# EVENT I/O KEYS
+MEASUREMENT_KEY = 'now'
+PREVIOUS_STATE_KEY = 'prior'
+LATITUDE_KEY = 'latitude'
+LONGITUDE_KEY = 'longitude'
+VARIANCE_KEY = 'variance'
+
+def get_variance( event ):
+    if 'origin' in event:
+        if 'cel' in event['origin']:
+            # if this is a cell tower, degreed the effective accuracy
+            return 1000;
+
+    return 1.0
+
+
+# possibly refactor to a more efficient version:
+# http://scottlobdell.me/2014/08/kalman-filtering-python-reading-sensor-input/
+
+def process_update( measure, variance, prev_state, prev_cov ):
+    return (measure, variance);
+
 
 def lambda_handler(event, context):
     # Log the received event
-    print("Received event: " + json.dumps(event, indent=2))
+    #   implement me!
+
+    # print("Received event:");
+    # for key, value in event.items():
+    #     print("    [{}] = {}".format( key, value))
 
     try:
-        # Call DescribeJobs
-        print("value1 = " + event['key1'])
-        print("value2 = " + event['key2'])
-        print("value3 = " + event['key3'])
-        return "Hello World!";
+        variance = get_variance( event )
+
+        if (PREVIOUS_STATE_KEY in event):
+            print("....detected previous state:")
+            prev = event[PREVIOUS_STATE_KEY];
+            prevMean = np.array([ prev[LATITUDE_KEY], prev[LONGITUDE_KEY]])
+            prevCovar = np.multiply( prev[VARIANCE_KEY], np.eye( S0.shape[0]) )
+
+            print('    ??: {} <= {}'.format( prevMean, prevCovar ))
+            kf = KalmanFilter(
+                initial_state_mean=prevMean,
+                initial_state_covariance=prevCovar,
+                em_vars=['transition_covariance', 'observation_covariance'],
+                transition_matrices=TRANSITION_MATRIX)
+
+            print('   ?? kf: '+str(kf))
+
+            curObs = np.array([ event[LATITUDE_KEY], event[LONGITUDE_KEY]]);
+            curCovar = np.multiply( event[VARIANCE_KEY], np.eye( len(curObs)))
+
+            nextMean, nextCovar = kf.filter_update(
+                prevMean,
+                prevCovar,
+                observation_matrix=OBSERVATION_MATRIX,
+                observation=curObs,
+                observation_covariance=curCovar)
+
+            print('   ?? next_state: '+str( prevMean))
+            print('   ?? next_covar: '+str( prevCovar))
+
+            return {
+                LATITUDE_KEY: 1.,
+                LONGITUDE_KEY: 1.,
+                VARIANCE_KEY: 0.,
+            }
+
+
+        else:
+            # if first point in sequence...
+            # just return the single location point, with corresponding accuracy
+            return {
+                LATITUDE_KEY: event[LATITUDE_KEY],
+                LONGITUDE_KEY: event[LONGITUDE_KEY],
+                VARIANCE_KEY: variance
+            }
+
     except Exception as e:
         print(e)
-        message = 'Error getting Batch Job status'
-        print(message)
-        raise Exception(message)
+        return { 'message': str(e) }
--- test_lambda.py
+++ test_lambda.py
@@ -8,47 +8,69 @@ import sys
 import unittest
 
 
-import lambda_code;
+from lambda_code import lambda_handler;
 
-import numpy;
+import numpy as np;
+from numpy.random import multivariate_normal as norm
 
+def createEvent( latitude, longitude, variance=1.0, prior={}, origin='gps' ):
+    result = {
+        'origin': origin,
+        'latitude': latitude,
+        'longitude': longitude,
+        'variance': variance};
+    if prior:
+        result['prior']= prior
 
+    return result;
 
 class IntentTestCase(unittest.TestCase):
 
-
     def setUp(self):
-        truth = ( 24, 45);
+        measurement_count = 20;
 
-        print( "Generating random test inputs, centered around "+str(truth));
-        # random generates data on [0,1)
-        error = numpy.random.rand( 20,2)
-        self.measurements = numpy.add( truth, error)
-        self.kf = lambda_code.lambda_handler
+        initial_truth = np.array([ 24, 45])
+
+        error_variance = 1.0;
+        error_covariance = np.multiply( error_variance, np.ones([2,2]));
+
+        # isn't numpy cool?
+        self.measurements = norm( mean= initial_truth, cov=error_covariance, size = measurement_count)
+
+    def test_initial_filter( self):
+        # pass in first point: without previous state continuity
+        #print( "    (" +str(self.measurements[0][0])+", "+str(self.measurements[0][1])+")")
+        event = createEvent( self.measurements[0][0], self.measurements[0][1] )
+
+        result = lambda_handler( event, None);
+
+        self.assertEqual( result['latitude'], self.measurements[0][0]);
+        self.assertEqual( result['longitude'], self.measurements[0][1]);
+        self.assertEqual( result['variance'], 1.0);
 
 
-        # numpy.savetxt( 'measurements.csv', measured, fmt="%6.4f", delimiter=',')
-
-
-    def test_simple_response( self):
-        self.assertEqual( self.measurements.shape, (20,2));
-
-# assertEqual(a, b) 	a == b
-# assertNotEqual(a, b) 	a != b
-# assertTrue(x) 	bool(x) is True
-# assertFalse(x) 	bool(x) is False
-# assertIs(a, b) 	a is b 	2.7
-# assertIsNot(a, b) 	a is not b 	2.7
-# assertIsNone(x) 	x is None 	2.7
-# assertIsNotNone(x) 	x is not None 	2.7
-# assertIn(a, b) 	a in b 	2.7
-# assertNotIn(a, b) 	a not in b 	2.7
-# assertIsInstance(a, b) 	isinstance(a, b) 	2.7
-# assertNotIsInstance(a, b) 	not isinstance(a, b) 	2.7
-
-    def test_first_call( self):
+    def test_next_call( self):
         pass
-        #self.kf()
+        # print( "running Kalman Filter update: ");
+        #
+        # # pass in first point: without previous state continuity
+        # #print( "    (" +str(self.measurements[0][0])+", "+str(self.measurements[0][1])+")")
+        # events=[];
+        # events.append( createEvent( self.measurements[0][0], self.measurements[0][1]))
+        # events.append( createEvent( self.measurements[1][0], self.measurements[1][1], prior = events[0]))
+        #
+        # # print("..event[0]: {}".format( events[0]));
+        # # print("..event[1]: {}".format( events[1]));
+        #
+        # result = lambda_handler( events[1], None);
+        #
+        # print( result );
+        #
+        # self.assertEqual( result['latitude'], self.measurements[0][0]);
+        # self.assertEqual( result['longitude'], self.measurements[0][1]);
+        # self.assertEqual( result['variance'], 1.0);
+        #
+        # print( result );
 
 
 
